4d33a3573f0c7f5ccbc857bf74eafa3759986c63
Yonathan Arroyave <yonatan.arroyave.rosero@gmail.com>
2024-11-16 17:29:48 -0500

    add icon

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { NavLink } from "react-router-dom"
-import { ShoppingCartContext} from "../Context"
+import { ShoppingBagIcon } from '@heroicons/react/24/solid'
+import { ShoppingCartContext } from "../Context"
 
 const Navbar = () => {
     const context = useContext(ShoppingCartContext)
@@ -93,8 +94,9 @@ const Navbar = () => {
                         sign in
                     </NavLink>
                 </li>
-                <li>
-                    🛒{context.count}
+                <li className='flex items-center'>
+                    <ShoppingBagIcon className='h-6 w-6 text-black'></ShoppingBagIcon>
+                    <div>{context.count}</div>
                 </li>
 
 
