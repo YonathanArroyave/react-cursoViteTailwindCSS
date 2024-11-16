42f399894fa30e9ad49240c9dc3da93ccf0ade2f
Yonathan Arroyave <yonatan.arroyave.rosero@gmail.com>
2024-11-16 01:31:34 -0500

    add card

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -3,12 +3,12 @@ const Navbar = () => {
     const activeStyle = 'underline underline-offset-4'
 
     return (
-        <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 text-sm font-light">
-            <ul className="flex items-center gap-3">
-                <li className="font-semibold text-lg">
+        <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
+            <ul className='flex items-center gap-3'>
+                <li className='font-semibold text-lg'>
                     <NavLink
                         to='/'
-                       >
+                    >
                         Shopi
                     </NavLink>
                 </li>
@@ -67,25 +67,25 @@ const Navbar = () => {
                 </li>
                 <li>
                     <NavLink to='/my-orders'
-                    className={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                    }>
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
                         My Orders
                     </NavLink>
                 </li>
                 <li>
                     <NavLink to='/my-count'
-                    className={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                    }>
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
                         My Account
                     </NavLink>
                 </li>
                 <li>
                     <NavLink to='/sign-in'
-                    className={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                    }>
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
                         sign in
                     </NavLink>
                 </li>
