429f259dbc9c81a3e93ed8c947a481059102c4db
Yonathan Arroyave <yonatan.arroyave.rosero@gmail.com>
2024-11-17 00:06:33 -0500

    add productDetail

--- src/Components/Context/index.jsx
+++ src/Components/Context/index.jsx
@@ -4,11 +4,23 @@ export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
   const [count, setCount] = useState(0)
+  // Product Detail · Open/Close
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+  const openProductDetail = () => setIsProductDetailOpen(true)
+  const closeProductDetail = () => setIsProductDetailOpen(false)
+   // Product Detail · Show product
+   const [productToShow, setProductToShow] = useState({})
 
   return (
     <ShoppingCartContext.Provider value={{
       count,
-      setCount
+      setCount,
+      openProductDetail,
+      closeProductDetail,
+      isProductDetailOpen,
+      productToShow,
+      setProductToShow
+
     }}>
       {children}
     </ShoppingCartContext.Provider>
