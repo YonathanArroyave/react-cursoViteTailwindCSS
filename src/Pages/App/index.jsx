c9feecacf2295ede3d46e98b012935784cf22bbb
Yonathan Arroyave <yonatan.arroyave.rosero@gmail.com>
2024-11-17 13:59:26 -0500

    acomodamos myorden para visualizar el ultimo pedido

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -16,6 +16,7 @@ const AppRoutes = () => {
     { path: '/my-count', element: <MyAcount /> },
     { path: '/my-order', element: <MyOrder /> },
     { path: '/my-orders', element: <MyOrders /> },
+    { path: '/my-orders/last', element: <MyOrder /> },
     { path: '/sign-in', element: <Signin /> },
     { path: '/*', element: <NotFound /> },
 
--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,8 +1,35 @@
+import { useContext } from "react"
+import { Link } from "react-router-dom"
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
+import { ShoppingCartContext } from "../../Components/Context"
 import Layout from "../../Components/Layout"
+import OrderCard from "../../Components/OrderCard"
+
+
 function MyOrder() {
+  const context = useContext(ShoppingCartContext)
+
   return (
     <Layout>
-      MyOrder
+      <div className='flex items-center justify-center relative w-80 mb-6'>
+        <Link to='/my-orders' className='absolute left-0'>
+          <ChevronLeftIcon className='h-6 w-6 text-black cursor-pointer' />
+        </Link>
+        <h1>My Order</h1>
+      </div>
+      <div className='flex flex-col w-80'>
+        {
+          context.order?.slice(-1)[0].products.map(product => (
+            <OrderCard
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              imageUrl={product.image}
+              price={product.price}
+            />
+          ))
+        }
+      </div>
     </Layout>
   )
 }
