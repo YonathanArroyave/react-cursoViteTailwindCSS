1f2252d1e308ae2746bdd5d0e99bd9b602614071
Yonathan Arroyave <yonatan.arroyave.rosero@gmail.com>
2024-11-17 14:56:25 -0500

    lista de ordenes

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,8 +1,27 @@
+import { useContext } from "react"
+import { Link } from "react-router-dom"
 import Layout from "../../Components/Layout"
+import { ShoppingCartContext } from "../../Components/Context"
+import OrdersCard from '../../Components/OrdersCard'
+import { XMarkIcon } from '@heroicons/react/24/solid'
+
 function MyOrders() {
+  const context = useContext(ShoppingCartContext)
+
   return (
     <Layout>
-      MyOrders
+      <div className='flex items-center justify-center relative w-80'>
+        <h1>My Orders</h1>
+      </div>
+      {
+        context.order.map((order, index) => (
+          <Link key={index} to={`/my-orders/${index}`}>
+            <OrdersCard
+              totalPrice={order.totalPrice}
+              totalProducts={order.totalProducts} />
+          </Link>
+        ))
+      }
     </Layout>
   )
 }
